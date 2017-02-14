e13204b894e6a7093460c921fc51686c79771790
coderunner09 <rampaysanjana8@gmail.com>
2017-02-14 14:31:22 +0530

    Create gif.js

---
+++ gif.js
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>
+Hello, It's me!
+</title>
+</head>
+<body>
+<div>
+Welcome to the black parade.
+</div>
+<h2>When I was a young boy, my father took me into the city to see a marching band.</h2>
+<br>
+<p>
+fml
+</p>
+<hr/>
+<p>
+Badabongbong
+</p>
+</body>
+</html>
